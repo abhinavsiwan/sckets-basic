dd4a90633ef60113c2e2c4ce05e082cf882e15d9
Abhinav Kumar <abhinavsiwan@gmail.com>
2016-06-24 11:30:05 -0700

    make two browers communicate to each other

--- server.js
+++ server.js
@@ -6,8 +6,23 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function () {
+io.on('connection', function (socket) {
 	console.log('User conected via socket.io');
+
+	//make two browsers communicate to each other
+	//listen to messages from the client
+	socket.on('message', function (message) {
+		console.log('Message received:' + message.text);
+
+		//io.emit() -> send the message to everyone including sender
+		//sends the message to everyone except the sender
+		socket.broadcast.emit('message', message);
+	});
+
+	//send message to the client
+	socket.emit('message', {
+		text: 'Welcome to the chat applcation!'
+	});
 });
 
 http.listen(PORT, function () {
