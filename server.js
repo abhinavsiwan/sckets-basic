9457dba8efdc10bd2ac4717ffdb36479467fdf3d
Abhinav Kumar <abhinavsiwan@gmail.com>
2016-06-24 16:26:05 -0700

    Added userCommand like @currentUsers

--- server.js
+++ server.js
@@ -9,13 +9,33 @@ var now = moment();
 var clientInfo = {};
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function (socket) {
+//send current users to provided socket
+function sendCurrentUsers(socket) {
+	var info = clientInfo[socket.id];
+	var users = [];
+
+	Object.keys(clientInfo).forEach(function (socketID) {
+		var userInfo = clientInfo[socketID];
+
+		if(info.room === userInfo.room) {
+			users.push(userInfo.name);
+		}
+	});
+
+	socket.emit('message', {
+		name: 'System',
+		text: 'Current Users:' + users.join(', '),
+		timestamp: now.valueOf()
+	});
+}
+
+io.on('connection', function(socket) {
 	console.log('User conected via socket.io');
 
 	//code to leave or disconnect from private rooms
-	socket.on('disconnect', function () {
+	socket.on('disconnect', function() {
 		var userData = clientInfo[socket.id]
-		if(typeof userData !== 'undefined') {
+		if (typeof userData !== 'undefined') {
 			socket.leave(userData.room);
 			io.to(userData.room).emit('message', {
 				name: 'System',
@@ -28,7 +48,7 @@ io.on('connection', function (socket) {
 	});
 
 	//code to join private rooms
-	socket.on('joinRoom' , function (req) {
+	socket.on('joinRoom', function(req) {
 		clientInfo[socket.id] = req;
 		socket.join(req.room);
 		socket.broadcast.to(req.room).emit('message', {
@@ -40,15 +60,17 @@ io.on('connection', function (socket) {
 
 	//make two browsers communicate to each other
 	//listen to messages from the client
-	socket.on('message', function (message) {
+	socket.on('message', function(message) {
 		console.log('Message received:' + message.text);
 
-		message.timestamp = now.valueOf();
-		//io.emit() -> send the message to everyone including sender
-		//sends the message to everyone except the sender
-		//socket.broadcast.emit('message', message);
-		//io.emit('message', message);
-		io.to(clientInfo[socket.id].room).emit('message', message);
+		if (message.text === '@currentUsers') {
+			sendCurrentUsers(socket);
+		} else {
+			message.timestamp = now.valueOf();
+			//socket.broadcast.emit('message', message);
+			//io.emit('message', message);
+			io.to(clientInfo[socket.id].room).emit('message', message);
+		}
 	});
 
 	//timeStamp property - Javascript timestamp(miliseconds)
@@ -61,6 +83,6 @@ io.on('connection', function (socket) {
 	});
 });
 
-http.listen(PORT, function () {
+http.listen(PORT, function() {
 	console.log('Server started');
 })
